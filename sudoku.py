7bc0845a21356d8185f37dd4f033bc599e80d08e
lwallaceos <lwallace756@students.ccsnh.edu>
2025-01-07 00:26:48 -0500

    file fix

--- sudoku.py
+++ sudoku.py
@@ -10,23 +10,18 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
 
-def solve(bo):
-    find = find_empty(bo)
-    if not find:
-        return True
-    else:
-        row, col = find
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
 
-    for i in range(1,10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
-
-            if solve(bo):
-                return True
-
-            bo[row][col] = 0
-
-    return False
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
 
             
